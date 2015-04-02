f487a26824bbed9b462c6f38e327c8756c8318cb
keliaMurata <kelia.murata@yahoo.com>
2015-04-01 21:03:49 -0600

    Seaweed incomplete

---
+++ src/ofApp.cpp
@@ -0,0 +1,90 @@
+/*
+	Kelia Murata
+	Seaweed Project
+	A Polyline quiz: creates a curved polyline that is shaped, colored, and has a 
+	swaying movement to mimic sea weed underwater.
+	Version 2.0, retry
+	Incomplete. 
+
+*/
+
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup()
+{
+	//declare points
+	basePoint = ofPoint(ofGetWindowWidth() /2, ofGetWindowHeight());
+	midPoint = ofPoint(ofGetWindowWidth() /2 - 100, ofGetWindowHeight() /2 - 150);
+	endPoint = ofPoint(ofGetWindowWidth() /2 + 40, 90);
+	finalPoint = ofPoint(ofGetWindowWidth() / 2, 10);
+
+	//create your polyline from your seaweed body 
+	seaweed.curveTo(basePoint);//doubled for anchor 
+	seaweed.curveTo(basePoint);
+	seaweed.curveTo(midPoint);
+	seaweed.curveTo(endPoint);
+	seaweed.curveTo(finalPoint);
+	seaweed.curveTo(finalPoint);
+
+}
+
+//--------------------------------------------------------------
+void ofApp::update()
+{
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw()
+{
+	ofBackground(100);
+	ofSetLineWidth(2.0);
+	ofSetColor(25, 220, 25);
+	seaweed.draw(); 
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
